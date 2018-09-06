928345d89169496f965e7ed8a1c4ff1882082e75
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-06 23:39:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 
 def speak_to_grandma(phrase_for_grandma)
-  if !phrase_for_grandma.capitalize
+  if !phrase_for_grandma.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase_for_grandma.capitalize
+  elsif phrase_for_grandma.upcase
     puts "NO, NOT SINCE 1938!."
   elsif phrase_for_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
